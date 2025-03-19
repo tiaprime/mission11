5892ecb670c725f82013b6350c99e3000baddedf
tiaprime <tiaprime@byu.edu>
2025-03-19 12:06:34 -0600

    data shown when backend is laucned

---
+++ backend/mission11/mission11/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using mission11.Data;
+
+namespace mission11.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookDbContext _bookContext;
+        public BookController(BookDbContext temp) => _bookContext = temp;
+
+        [HttpGet("AllBooks")]
+        public IEnumerable<Book> GetBooks()
+        {
+            var x = _bookContext.Books
+                .ToList();
+
+            return (x);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
--- backend/mission11/mission11/Program.cs
+++ backend/mission11/mission11/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using mission11.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<BookDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("BookConnection")));
+
+//builder.Services.AddCors();
+
 
 var app = builder.Build();
 
